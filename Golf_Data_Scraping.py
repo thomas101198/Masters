ab88afc77b2362ea7f9ad57e6c3d2ae8fc892fa3
thomas101198 <thomas.w.lowe@sky.com>
2024-05-12 14:08:05 +0100

    Scrape_PGA_Website

--- Golf_Data_Scraping.py
+++ Golf_Data_Scraping.py
@@ -1,22 +1,38 @@
 import pandas as pd
-from bs4 import BeautifulSoup
-import requests
 
-data = {
+""" data = {
   "calories": [420, 380, 390],
   "duration": [50, 40, 45]
-}
+} """
 
-df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+""" df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+ """
 
 
+URL = "https://www.pgatour.com/tournaments/2023/masters-tournament/R2023014/past-results"
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
 
-URL = "https://www.flashscore.co.uk/golf/pga-tour/masters-tournament-2023/" 
-r = requests.get(URL) 
+driver = webdriver.Chrome()
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-""" print(soup.prettify()) """ 
+# load page
+driver.get(URL)
 
-mydivs = soup.find_all("title", {"class": "Click for player card!"}) 
-print(mydivs)
+# get table
+table = driver.find_element(By.CSS_SELECTOR, "table.chakra-table")
+assert table, "table not found"
+
+# remove empty rows
+driver.execute_script("""arguments[0].querySelectorAll("td.css-1au52ex").forEach((e) => e.parentElement.remove())""", table)
+
+# get html of the table
+table_html = table.get_attribute("outerHTML")
+
+# quit selenium
+driver.quit()
+
+df = pd.read_html(table_html)[0]
+
+print(df)
