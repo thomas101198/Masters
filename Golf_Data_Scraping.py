e65936cc4a003b8f7ed4dd4492ded44c4b3c4684
thomas101198 <thomas.w.lowe@sky.com>
2024-05-23 20:49:15 +0100

    Code now loops through 2014-2024 leaderboards and successfully publishes names to database

--- Golf_Data_Scraping.py
+++ Golf_Data_Scraping.py
@@ -10,63 +10,69 @@ mydb = mysql.connector.connect(
   password = "dHBiujZhFa",
   database = "sql8705870"
 )
-mycursor = mydb.cursor()
-#goes back to 2014. Need to wrap the url in an if statement and then change the insert part into a function
-URL = "https://www.pgatour.com/tournaments/2022/masters-tournament/R2018014/leaderboard"
 
+mycursor = mydb.cursor()
 driver = webdriver.Chrome()
 
-# load page
-driver.get(URL)
+def load_data(URL,y):
+  driver.get(URL)
+  table = driver.find_element(By.CSS_SELECTOR, "table.chakra-table")
+  assert table, "table not found"
+  df = pd.read_html(driver.find_element(By.CSS_SELECTOR, "table.chakra-table").get_attribute('outerHTML'))[0]
+  df = df.drop(df.columns[12:], axis=1)
+  df2 = df.loc[:,"Unnamed: 2_level_0"]
+  upload_data(df2,y)
 
-# get table
-table = driver.find_element(By.CSS_SELECTOR, "table.chakra-table")
-assert table, "table not found"
+def upload_data(players,b):
+  existing_players = []
+  mycursor.execute('SELECT CONCAT(first_name," ",last_name) FROM Masters_Players') 
+  captured_players = mycursor.fetchall() 
+  for x in captured_players:
+    existing_players.append(x[0])
+  
+  y = 0
 
-df = pd.read_html(driver.find_element(By.CSS_SELECTOR, "table.chakra-table").get_attribute('outerHTML'))[0]
+  for i in players['Player']:
+    if type(i) == str:
+      if len(i.split()) <= 4:
+        i = i.replace("(a)","")
+        if i not in existing_players:
+          try:
 
-df = df.drop(df.columns[12:], axis=1)
+            param1 =  i.split(" ", 1)[0]
+            param2 = i.split(" ", 1)[1]
 
-""" for col in df.columns:
-    print(col) """
+            cursor = mydb.cursor()
+            cursor.execute("""INSERT INTO Masters_Players (first_name,last_name) VALUES (%s,%s)""", (param1, param2))
+            mydb.commit()
+            print(cursor.rowcount, "Record inserted successfully into Laptop table")
+            cursor.close()
+            y += 1
+          except mysql.connector.Error as error:
+            print("Failed to insert record into Laptop table {}".format(error))
+  print("Total players added:",y,"Year:",b)
 
-df2 = df.loc[:,"Unnamed: 2_level_0"]
+#These are the two variables we neeed to change
+start_year = (2014)
+end_year = (2024)
+
+url = ""
+
+for a in range(start_year,end_year+1):
+  url = f"https://www.pgatour.com/tournaments/2022/masters-tournament/R{a}014/leaderboard" 
+  load_data(url,a)
+
+
+
+
 
 
 # We needed to remove NaN values, where adverts or gaps appear in the tables. Then remove any 'names' larger than 4 words long as we were capturing a message about the cut. 
 # Finally we want to remove the marker for amateur from the surname
-existing_players = []
-mycursor.execute('SELECT CONCAT(first_name," ",last_name) FROM Masters_Players') 
-captured_players = mycursor.fetchall() 
-for x in captured_players:
-  existing_players.append(x[0])
 
-# Need to fix the below as 'O'Meara' didnt insert due to escaped character
-y = 0
 
-for i in df2['Player']:
-  if type(i) == str:
-    if len(i.split()) <= 4:
-      i = i.replace("(a)","")
-      if i not in existing_players:
-        try:
 
-          param1 =  i.split(" ", 1)[0]
-          param2 = i.split(" ", 1)[1]
 
-          cursor = mydb.cursor()
-          cursor.execute("""INSERT INTO Masters_Players (first_name,last_name) VALUES (%s,%s)""", (param1, param2))
-          mydb.commit()
-          print(cursor.rowcount, "Record inserted successfully into Laptop table")
-          cursor.close()
-          y += 1
-        except mysql.connector.Error as error:
-          print("Failed to insert record into Laptop table {}".format(error))
-
-print("Total entries added:",y)
-# The names are then useed to populate the player first name and last name column in the Masters_Players table. We can get the rest of the player info later
-
-# df.to_sql(con=con, name='table_name_for_df', if_exists='replace', flavor='mysql')
 
 
 
